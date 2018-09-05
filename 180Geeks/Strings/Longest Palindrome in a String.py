ac69aa0f42e72e643cb608800b3f42e4ab72d3fc
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-05 17:32:09 -0500

    update

--- 180Geeks/Strings/Longest Palindrome in a String.py
+++ 180Geeks/Strings/Longest Palindrome in a String.py
@@ -1,1 +1,13 @@
 # Given a string S, find the longest palindromic substring in S.
+
+a = [1,2,3]
+n = len(a)
+
+def _rec(a, n):
+    if len(a) == 1:
+        return a[0]
+
+    else:
+        return a[0] + _rec(a[1:], n)
+
+print(_rec(a,n))
