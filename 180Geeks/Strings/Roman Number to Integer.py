8af5366142b2f363642ff986da809d43f580ad65
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-18 16:05:11 -0500

    string

--- 180Geeks/Strings/Roman Number to Integer.py
+++ 180Geeks/Strings/Roman Number to Integer.py
@@ -1,14 +1,40 @@
 # Given an string in roman no format (s)  your task is to convert it to integer .
 
-roman = 'V'
+roman = 'MMMCMCCIV'
 
 dict = {
     'I': 1,
     'II': 2,
     'III': 3,
     'IV': 4,
-    'V': 5
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
 }
 
-def convert(roman):
-    pass
+def convert(roman, dict):
+    roman_int = 0
+    i = 0
+    while i < len(roman):
+        value1 = dict[roman[i]]
+
+        if (i+1) < len(roman):
+            value2 = dict[roman[i+1]]
+
+            if value1 >= value2:
+                roman_int += value1
+                i += 1
+            else:
+                roman_int += value2 - value1
+                i += 2
+        else:
+            roman_int += value1
+            i += 1
+
+    return roman_int
+
+print(f"Given roman number: {roman}")
+print(f"Roman number in integer: {convert(roman, dict)}")
