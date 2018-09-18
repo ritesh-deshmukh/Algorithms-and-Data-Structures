c8f134ad5b32fd68806e1c8ec34e5aa0bba17d7e
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-18 18:18:43 -0500

    string

---
+++ 180Geeks/Strings/Remove Duplicates.py
@@ -0,0 +1,16 @@
+# Given a string, remove duplicates from it.
+# Note that original order of characters must be kept same.
+# Expected time complexity O(n) where n is length of input string and extra space O(1) under the assumption that there are total 256 possible characters in a string.
+
+word = "aabbccdd"
+
+def remove_duplicates(word):
+    word = list(word)
+    i = 0
+    while i < len(word):
+        if word.count(word[i]) > 1:
+            del(word[i])
+            i += 1
+    print(word)
+
+remove_duplicates(word)
